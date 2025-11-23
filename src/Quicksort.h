9001e8abb468e32b614b39423089b6d722f9eec0
lzoerner <leon_andre.zoerner@stud.htwk-leipzig.de>
2025-11-23 03:06:19 +0100

    -Quicksort::quicksortParallel

--- src/Quicksort.h
+++ src/Quicksort.h
@@ -10,14 +10,14 @@ public:
     // Funktionen
     Quicksort();
     void sort(int *liste, int lange);
-    void sortParallel(int *liste, int lange);
+    // void sortParallel(int *liste, int lange);
 
     // public Static
 
 private:
     // Funktionen
     static void quicksort(int *liste, int links, int rechts, Position *pos);
-    static void quicksortParallel(int *liste, const int links, const int rechts);
+    // static void quicksortParallel(int *liste, const int links, const int rechts);
     static void partitioniere(int *liste, int links, int rechts, int &ml, int &mr);
     static void vertausche(int *liste, int a, int b);
 };
